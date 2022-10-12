bd6bc93be85d604c911e3e6b1bbf33ba727d08a6
Madhu-02522 <madhavikollampally1997@gmail.com>
2022-10-12 12:50:05 -0500

    first commit

---
+++ src/main/java/com/tekleaders/tops/TopsWebApplication.java
@@ -0,0 +1,13 @@
+package com.tekleaders.tops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopsWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tekleaders/tops/TopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekleaders.tops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
